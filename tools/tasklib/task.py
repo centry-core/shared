78d5134249bfeec2c96099fa471d464dddbf1aa5
Usmonbek Ravshanov <usmonbek_ravshanov@epam.com>
2023-09-12 16:02:06 +0500

    New task registering with meta

--- tools/tasklib/task.py
+++ tools/tasklib/task.py
@@ -1,54 +1,138 @@
-from typing import Dict, Type
+import re
+from typing import Optional
 from types import FunctionType
 from collections import namedtuple
 from pylon.core.tools import log
 
-
-_data_tuple = namedtuple("Data", ("obj", "params", "display_name"))
-_tasks_registry = dict()
-#Dict[str, NamedTuple[Union[FunctionType, Type]]]
+from pydantic import ValidationError, BaseModel
 
 
-def register_task(name: str, func_obj: FunctionType, parameters: Dict, display_name:str=None):
-    if name in _tasks_registry:
-        log.error(f"Task with {name} name already exists")
+_data_tuple = namedtuple("Data", ("obj", "meta"))
+_tasks_registry: dict = {}
+_reverse_task_registy: dict = {}
+
+
+class TaskMeta(BaseModel):
+    uid: str
+    tooltip: Optional[str]
+    icon_url: Optional[str]
+
+
+def register_task(rpc_name: str, func_obj: FunctionType, meta: dict):
+    if rpc_name in _tasks_registry:
+        log.error(f"Task with {rpc_name} name already exists")
         return False
     
     if not isinstance(func_obj, FunctionType):
         log.error("Function object is not passed")
         return False
     
-    if display_name is None:
-        display_name = ' '.join(tuple(map(lambda x:x.capitalize(), name.split("_")[1:])))
+    front_name_id = meta['uid']
 
-    _tasks_registry[name] = _data_tuple(func_obj, parameters, display_name)
-    log.info(f"Registering function with name {name}")
+    _tasks_registry[rpc_name] = _data_tuple(func_obj, meta)
+    _reverse_task_registy[front_name_id] = rpc_name
+    log.info(f"Registering function with name {rpc_name}")
     return True
 
 
 def list_tasks_meta():
-    return {name:(entry.params, entry.display_name) for name, entry in _tasks_registry.items()}
+    return {
+        entry.meta for entry in _tasks_registry.values() 
+    }
 
-
-def get_task_meta(name:str):
-    entry = _tasks_registry.get(name)
-    if not entry:
-        log.error(f"Task with {name} not found")
-        return 
-    return entry.params, entry.display_name 
-
-
-def get_task_object(name:str):
-    entry = _tasks_registry.get(name)
-    if not entry:
-        log.error(f"Task with {name} not found")
-        return
-    return entry.obj, entry.display_name
+def get_tasks_to_rpc_mappings():
+    return _reverse_task_registy
 
 
 ### Decorators
-def task(name:str, params:Dict[str, Type]):
+def task(rpc_name:str, meta_payload):
     def _decorator(obj: FunctionType):
-        register_task(name, obj, params)
+        try:
+            meta = TaskMeta.validate(meta_payload)
+        except ValidationError as e:
+            log.error(f'Falid to register flowy task\nReason: {e}')
+            return obj
+        
+        register_task(rpc_name, obj, meta.dict())
         return obj
     return _decorator
+
+
+
+def handle_exceptions(fn):
+
+    def _is_special_value(value):
+
+        if not isinstance(value, str):
+            return False
+        
+        variable_pattern = r"([a-zA-Z0-9_]+)"
+        variables_pattern = r"{{variables\." + variable_pattern + r"}}"
+        prev_pattern = r"{{nodes\['"+ variable_pattern + r"'\]\.?" + variable_pattern + r"?}}"
+
+        if re.fullmatch(variables_pattern, value) or \
+            re.fullmatch(prev_pattern, value):
+            return True
+
+        return False
+
+    def decorated(self, **kwargs):
+        try:
+            fn(self, **kwargs)
+            return {"ok": True}
+        except ValidationError as e:
+            valid_erros = []
+            for error in e.errors():
+                log.info(f"ERROR: {error}")
+                if error['type'] == "value_error.missing" or "__root__" in error['loc']:
+                    valid_erros.append(error)
+                    continue
+                
+                invalid_value = {**kwargs}
+                for loc in error["loc"]:
+                    invalid_value = invalid_value[loc]
+                
+                # check for special values
+                if not _is_special_value(invalid_value):
+                    valid_erros.append(error)
+
+            if valid_erros:
+                return {"ok": False, "errors": valid_erros}
+            return {"ok": True}
+        except Exception as e:
+            return {"ok": False, "error": str(e)}
+        
+    return decorated
+
+
+# from typing import Optional
+# from types import FunctionType
+# from ..rpc_tools import EventManagerMixin
+# from pylon.core.tools import log
+# from pydantic import ValidationError, BaseModel
+
+
+# class TaskMeta(BaseModel):
+#     uid: str
+#     tooltip: Optional[str]
+#     icon_url: Optional[str]
+
+
+# ### Decorators
+# def task(rpc_name:str, meta_payload):
+#     def _decorator(obj: FunctionType):
+#         try:
+#             meta = TaskMeta.validate(meta_payload)
+#         except ValidationError as e:
+#             log.error(f'Falid to register flowy task\nReason: {e}')
+#             return obj
+
+#         event_manager = EventManagerMixin().event_manager
+#         payload = {
+#             'rpc_name': rpc_name,
+#             'meta': meta.dict(),
+#         }
+#         log.info(f"BEFORE EVENT : {payload}")
+#         event_manager.fire_event("register_task", payload)
+#         return obj
+#     return _decorator
