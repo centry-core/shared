974e1fbfbbca903ca633e9f9d24d542f7b989c8e
Ivan Krakhmaliuk <Ivan_Krakhmaliuk@epam.com>
2023-12-08 17:52:16 +0200

    next mock

--- tools/secret_engines/mock.py
+++ tools/secret_engines/mock.py
@@ -55,7 +55,7 @@ class MockEngine(metaclass=MockMeta):
     def create_project_space(self, *args, **kwargs):
         log.info("create_project_space(%s, %s)", args, kwargs)
         result = Holder()
-        result.dict = lambda: {}
+        result.dict = lambda x: {}
         return result
 
     def get_all_secrets(self, *args, **kwargs):
