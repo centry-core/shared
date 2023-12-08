0bb66d8e7314f3e6605f8023ed4ab161d884c8ec
Ivan Krakhmaliuk <Ivan_Krakhmaliuk@epam.com>
2023-12-08 13:29:19 +0200

    autmatically create base schema

--- tools/db.py
+++ tools/db.py
@@ -1,11 +1,15 @@
 from contextlib import contextmanager
 from flask_sqlalchemy import BaseQuery
+import sqlalchemy
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker, scoped_session, declarative_base
 
 from tools import config as c
 
 engine = create_engine(c.DATABASE_URI, **c.DATABASE_ENGINE_OPTIONS)
+if not engine.dialect.has_schema(engine, c.POSTGRES_SCHEMA):  # TODO: allow to use other DBs, not only postgres
+    engine.execute(sqlalchemy.schema.CreateSchema(c.POSTGRES_SCHEMA))
+#
 session = scoped_session(sessionmaker(bind=engine))
 Base = declarative_base()
 Base.query = session.query_property(query_cls=BaseQuery)
