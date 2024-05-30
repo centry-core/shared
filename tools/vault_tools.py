3011a1c04a60b224b0a129512e539af1a0d9be7f
Aspect13 <ad13box@gmail.com>
2024-05-30 12:36:21 +0400

    adding type to node emitter

--- tools/vault_tools.py
+++ tools/vault_tools.py
@@ -23,7 +23,7 @@ from typing import Optional, Any, Union, List, Tuple
 
 import hvac  # actually comes from pylon
 from hvac.exceptions import InvalidRequest
-from pydantic import BaseModel, constr, ValidationError
+from pydantic import BaseModel, constr, ValidationError, SecretField
 
 from pylon.core.tools import log
 from jinja2 import Template, Environment, nodes
@@ -447,3 +447,24 @@ else:
     except:  # pylint: disable=W0702
         log.exception("Failed to set secrets engine: %s", c.SECRETS_ENGINE)
         raise
+
+
+class SecretString(SecretField):
+    from_secrets: bool = True
+    value: str
+
+    def __init__(self, value: str):
+        self._secret_value = value
+
+    def get_secret_value(self) -> Any:
+        return
+
+    def unsecret(self, project_id: Optional[int] = None):
+        if self.from_secrets:
+            if project_id:
+                client = VaultClient.from_project(project_id)
+            else:
+                client = VaultClient()
+            return client.unsecret(self.value)
+        else:
+            return self.value
