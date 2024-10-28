108e994d093bce163363fd1b012b398545af37e2
Aspect13 <ad13box@gmail.com>
2024-10-28 15:43:11 +0400

    various fixes

--- tools/integration_tools.py
+++ tools/integration_tools.py
@@ -41,12 +41,65 @@ def _find_first_configuration_by_partial_settings(
             return i
 
 
+def _find_config(
+        project_id: int,
+        configuration_personal: bool,
+        integration_name: str,
+        partial_settings: dict,
+        configurations: list,
+):
+    rpc_call = rpc_tools.RpcMixin().rpc.call
+
+    if configuration_personal:
+        integrations = [i for i in configurations if i.get('name') == integration_name]
+    else:
+        integrations = rpc_call.integrations_get_all_integrations_by_name(
+            project_id,
+            integration_name
+        )
+
+    for i in integrations:
+        for k, v in partial_settings.items():
+            if i.settings.get(k) != v:
+                break
+        else:
+            return i
+
+
 class ExternalIntegrationSupport(BaseModel):
-    configuration_personal: bool
-    configuration_title: str
+    configuration_personal: bool = False
+    configuration_title: str = None
 
     _integration_name = None
     _integration_fields = None
+
+    def dict_expand_from_configurations(self, project_id: int, personal_configurations: list, **kwargs) -> dict:
+        base_dict = super().dict(**kwargs)
+        base_dict.pop('configuration_personal', None)
+        base_dict.pop('configuration_title', None)
+
+        if not isinstance(self._integration_fields, dict):
+            integration_fields = {x: x for x in self._integration_fields}
+        else:
+            integration_fields = self._integration_fields
+
+        partial_settings = {
+            'title': self.configuration_title
+        }
+        integration = _find_config(
+            project_id=project_id,
+            configuration_personal=self.configuration_personal,
+            integration_name=self._integration_name,
+            partial_settings=partial_settings,
+            configurations=personal_configurations
+        )
+        if integration is None:
+            raise ValueError(f"Integration with title={self.configuration_title}' does not exist")
+
+        for field, integration_field in integration_fields.items():
+            base_dict[field] = integration.settings.get(integration_field)
+
+        return base_dict
 
     def dict_integration_expanded(self, user_id: int, project_id, **kwargs):
         """ Returns dict values with expanded external integration settings if exist"""
