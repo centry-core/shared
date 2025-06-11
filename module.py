75a57b6ed0e6c3047fe90dbc9ecea588572abd4a
Ivan Krakhmaliuk <Ivan_Krakhmaliuk@epam.com>
2025-06-11 14:46:06 +0300

    allow to disable shared/project apply via config

--- module.py
+++ module.py
@@ -141,27 +141,29 @@ class Module(module.ModuleModel):
         """ Ready callback """
         from .tools import db  # pylint: disable=C0415
         #
-        log.info("Getting shared metadata")
-        shared_metadata = db.get_shared_metadata()
+        if self.descriptor.config.get("apply_shared_metadata", True):
+            log.info("Getting shared metadata")
+            shared_metadata = db.get_shared_metadata()
+            #
+            log.info("Applying shared metadata")
+            with db.get_session(None) as shared_db:
+                shared_metadata.create_all(bind=shared_db.connection())
+                shared_db.commit()
         #
-        log.info("Applying shared metadata")
-        with db.get_session(None) as shared_db:
-            shared_metadata.create_all(bind=shared_db.connection())
-            shared_db.commit()
-        #
-        log.info("Getting project metadata")
-        tenant_metadata = db.get_tenant_specific_metadata()
-        #
-        log.info("Getting project list")
-        project_list = self.context.rpc_manager.timeout(120).project_list(
-            filter_={"create_success": True},
-        )
-        #
-        for project in project_list:
-            log.info("Applying project metadata: %s", project)
-            with db.get_session(project["id"]) as tenant_db:
-                tenant_metadata.create_all(bind=tenant_db.connection())
-                tenant_db.commit()
+        if self.descriptor.config.get("apply_project_metadata", True):
+            log.info("Getting project metadata")
+            tenant_metadata = db.get_tenant_specific_metadata()
+            #
+            log.info("Getting project list")
+            project_list = self.context.rpc_manager.timeout(120).project_list(
+                filter_={"create_success": True},
+            )
+            #
+            for project in project_list:
+                log.info("Applying project metadata: %s", project)
+                with db.get_session(project["id"]) as tenant_db:
+                    tenant_metadata.create_all(bind=tenant_db.connection())
+                    tenant_db.commit()
 
     def deinit(self):
         """ De-init module """
